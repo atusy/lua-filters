605ebd974a356c2251488351606a106f2bc27b2d
atusy <30277794+atusy@users.noreply.github.com>
2020-05-29 21:11:20 +0900

    refactor

--- number-sections/number-sections.lua
+++ number-sections/number-sections.lua
@@ -1,14 +1,14 @@
 -- For the formats not supporting the number-sections option
-lvls = {0, 0, 0, 0, 0, 0, 0, 0, 0}
+header_levels = {0, 0, 0, 0, 0, 0, 0, 0, 0}
 
 function Header(elem)
-  lvls[elem.level] = lvls[elem.level] + 1
-  local lvl = ""
+  header_levels[elem.level] = header_levels[elem.level] + 1
+  local level = ""
   for i = elem.level,1,-1 do
-    lvl = lvls[i] .. "." .. lvl
+    level = header_levels[i] .. "." .. level
   end
-  lvl = lvl .. " "
-  content = {pandoc.Str(lvl)}
+  level = level .. " "
+  content = {pandoc.Str(level)}
   for i = 1,#elem.content do
     content[i + 1] = elem.content[i]
   end
