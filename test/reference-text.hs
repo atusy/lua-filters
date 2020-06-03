bd6fa71009a93cfa3ba8db749820fc989ba02aae
atusy <30277794+atusy@users.noreply.github.com>
2020-06-03 23:50:20 +0900

    add test input and output for reference-text.lua

---
+++ test/reference-text.hs
@@ -0,0 +1,3 @@
+[Para [Str "1:",Space,Str "foo"]
+,Para [Str "1",Space,Str "and",Space,Str "2:",Space,Str "foo",Str "bar"]
+,Para [Str "3:",Space,Str "foo",SoftBreak,Str "bar"]]
---
+++ test/reference-text.md
@@ -0,0 +1,13 @@
+(ref:1) foo
+
+(ref:2) bar
+
+(ref:3) foo
+bar
+
+
+1: (ref:1)
+
+1 and 2: (ref:1)(ref:2)
+
+3: (ref:3)
