19cc9b85c3c9b9a16b66f885956ae2c5a9341a01
atusy <30277794+atusy@users.noreply.github.com>
2020-06-03 23:47:47 +0900

    improve reference-text.lua for CJK

--- lua/reference-text.lua
+++ lua/reference-text.lua
@@ -1,11 +1,19 @@
 local reference_text_table = {}
-local reference_text_pattern = "^%(ref:([%a%d-]+)%)$"
+local patterns = {
+  completing = "^%(ref:([%a%d-]+)%)$",
+  containing = "(%(ref:[%a%d-]+%))",
+  containing_escaped = "\\%(ref:([%a%d-]+)\\%)"
+}
+
+local function escape_symbol(text)
+  return(text:gsub("([\\`*_{}%[%]%(%)>#+-.!])", "\\%1"))
+end
 
 local function curate_refs(para)
-  local first_element = para.content[1]
-  local second_element = para.content[2]
-  if (first_element.tag == "Str") and second_element and (second_element.tag == "Space") then
-    local matched = first_element.text:match(reference_text_pattern)
+  local e1 = para.content[1]
+  local e2 = para.content[2]
+  if (e1.tag == "Str") and e2 and (e2.tag == "Space") then
+    local matched = e1.text:match(patterns["completing"])
     local n = #para.content
     local reference_text = {}
     if matched and (n > 2) then
@@ -21,15 +29,25 @@ local function curate_refs(para)
   end
 end
 
+local function split_refs(str)
+  local text = str.text:gsub(patterns["containing"], " %1 ")
+  text_table = {}
+  for t in text:gmatch("([^%s]+)") do
+    table.insert(text_table, pandoc.Str(t))
+  end
+  return text_table
+end
+
 local function solve_refs(str)
-  local matched = str.text:match(reference_text_pattern)
+  local matched = str.text:match(patterns["completing"])
   if matched then
-    return(reference_text_table[matched])
+    return reference_text_table[matched]
   end
 end
 
 reference_text = {
   {Para = curate_refs},
+  {Str = split_refs},
   {Str = solve_refs}
 }
 
