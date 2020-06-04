f95fdd27702fa88a8eee8b420c77cc534ec37dee
atusy <30277794+atusy@users.noreply.github.com>
2020-06-04 23:43:41 +0900

    add auto-number-lines.lua

---
+++ lua/auto-number-lines.lua
@@ -0,0 +1,22 @@
+local function CodeBlock(code)
+  local _, index = code.classes:find('un-numberLines')
+  if index then
+    local classes = {}
+    for i,j in code.classes do
+      if i ~= idex then
+        table.insert(classes, j)
+      end
+    end
+    code.classes = classes
+    return(code)
+  end
+
+  code.classes[#code.classes+1] = 'numberLines'
+  return(code)
+end
+
+number_lines_code_blocks = {
+  { CodeBlock = CodeBlock }
+}
+
+return number_lines_code_blocks
