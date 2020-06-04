0ce703f02b9af7b48fd7a91ad57937969bd7c808
atusy <30277794+atusy@users.noreply.github.com>
2020-06-04 22:53:37 +0900

    add test script

---
+++ test/test.sh
@@ -0,0 +1,31 @@
+#! /bin/sh
+
+stashed=$(git stash)
+
+function postprocess () {
+  git reset --hard --quiet
+  if [ "$stashed" != "No local changes to save" ]
+  then
+    git stash pop --quiet
+  fi;
+}
+
+markdowns=$(find test/*.md)
+for md in $markdowns
+do
+  hs=$(echo $md | sed s/md$/hs/)
+  lua_filter=$(echo $md | sed s/^test/lua/ | sed s/md$/lua/)
+  pandoc $md -t native -o $hs
+done
+
+git_diff=$(git diff)
+
+if [ "$git_diff" = "" ]
+then
+  postprocess
+  exit 0
+else
+  echo "$git_diff"
+  postprocess
+  exit 1
+fi;
