b1e9043bcdcec9693a15dcc38398e4c9ed3538c0
atusy <30277794+atusy@users.noreply.github.com>
2020-08-25 20:36:42 +0900

    update docs

--- lua/bookdown-reference-text.lua
+++ lua/bookdown-reference-text.lua
@@ -1,5 +1,32 @@
--- A filter that mimics bookdown's reference text
--- cf. https://github.com/rstudio/bookdown/
+--[[
+bookdown-reference-text - Reference text feature inspired by 'bookdown'
+
+cf. https://github.com/rstudio/bookdown/
+
+---
+crossref:
+  labels:
+    fig: "Fig. "
+    tab: "Tab. "
+    eq: "Eqn. "
+  link: true
+  number_sections: false # TODO: NOT WORKING!!
+---
+
+In case `number_sections: true`, and using HTML/EPUB,
+`--number-sections` (or the `-N`) option should also be enabled.
+
+# MIT License
+
+Copyright (c) 2020 Atsushi Yasumoto
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+]]
+
 local reference_text_table = {}
 local patterns = {
   completing = "^%(ref:([%a%d-]+)%)$",
--- lua/number-sections.lua
+++ lua/number-sections.lua
@@ -1,11 +1,18 @@
 --[[
-Number sections for formats without --number-sections
-A meta data option is `number_sections_with_attributes`,
-which is by default `true` except for markdown FORMAT.
+number-sections - Number sections like the --number-sections option
 
----
-number_sections_with_attributes: false
----
+If the "data-number" attribute is not required, opt it out by specifying `false`
+to the `number_sections_with_attributes` metadata.
+
+# MIT License
+
+Copyright (c) 2020 Atsushi Yasumoto
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ]]
 local full_attributes = FORMAT ~= "markdown"
 local section_number_table = {0, 0, 0, 0, 0, 0, 0, 0, 0}
